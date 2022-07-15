4a9db0f7854cc551b44592292ceb2114ec1bdded
deevenamekala1994 <deevenamekala1994@gmail.com>
2022-07-15 17:14:10 +0530

    objects

---
+++ objects.js
@@ -0,0 +1,15 @@
+//declaring an objects//
+const person ={name: "devi",age: "22",eyecolour:"blue"};
+
+console.log(person);
+
+
+const phone = {
+    brand:"samsung",
+    model:"m1",
+    ram:"8gb",
+    price:"10000",
+}
+
+console.log(phone);
+console.log(`${phone.brand} ${phone.ram}`);
