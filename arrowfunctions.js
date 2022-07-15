f5d3f3b9a29205ec81026ca85ca5a658885b8adb
deevenamekala1994 <deevenamekala1994@gmail.com>
2022-07-15 17:11:02 +0530

    arrow functions

---
+++ arrowfunctions.js
@@ -0,0 +1,36 @@
+//declaring the function used an const//
+const usr = ()=>console.log('hello');
+
+usr(10);
+ 
+const name =(num1)=>{      //this key word always represents the object that defined the arrow function// 
+    return num1+2;        //adding//
+}
+
+output = name (4);
+console.log(output);
+
+const sample = (num2)=>{
+    return num2-22;      //subtraction//
+}
+result1 = sample(15);
+console.log(result1)
+
+const data = (num3)=>{
+    return num3*6;
+}
+result2 = data(9);
+console.log(result2)
+
+const fun = (num4)=>{
+    return num4/3;
+}
+result3 = fun(9);
+console.log(result3);
+
+const per = (num5)=>{
+    return num5%6;
+}
+result4 = per(68);
+console.log(result4)
+
