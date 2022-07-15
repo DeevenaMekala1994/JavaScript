854605833f8d0b1771240d9b829d35e4f21b5185
deevenamekala1994 <deevenamekala1994@gmail.com>
2022-07-15 16:55:43 +0530

    string operations

---
+++ strings.js
@@ -0,0 +1,36 @@
+const str = 'i am deevena from nellore';
+
+//ex for split
+console.log(str.split(' '));
+
+//ex for trim
+console.log(str.trim());
+
+//ex for slice  //slice is also known as sub string
+console.log(str.slice(3,11));
+
+//ex for replace
+console.log(str.replace('dee','pra'));
+
+//ex for uppercase
+console.log(str.toUpperCase());
+
+//ex for lowercase
+console.log(str.toLowerCase());
+
+//example2//
+
+const str1 = '    i am from challapalli'
+
+console.log(str1.split(' '));
+
+console.log(str1.trim(' '));
+
+console.log(str1.slice(5,11));
+
+console.log(str1.replace('challa','anaka'));
+
+console.log(str1.toUpperCase());
+
+conasole.log(str1.toLowerCase());//
+
